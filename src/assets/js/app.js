159e84bbcf4e5435c72f31d0f7b0a257cdc14c2f
Vitaliy Fedoruk <wfedoruk80@gmail.com>
2021-09-06 09:51:53 +0300

    fixed 6.09

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -7,6 +7,7 @@ $(".background_gold, .menu_button").click(function() {
   if($('.nav_show').css('display') == 'block')
     {
       $('.background_gold').addClass('clicked');
+      $('.arrow_nav').css('content','none');
       
     } 
 });
@@ -20,7 +21,7 @@ $(document).on('click', function(e) {
   if($('.nav_show').css('display') == 'none')
     {
       $('.background_gold').removeClass('clicked');
-      
+      $('.arrow_nav').css('content','../images/arrow_right.png');
     } 
 });
 
@@ -28,18 +29,18 @@ $('.close_button').click(function()  {
    
   $('.nav_show').hide();
     
-})
+});
 
 
-const screenWidth = window.screen.width
-const screenHeight = window.screen.height
+// const screenWidth = window.screen.width
+// const screenHeight = window.screen.height
 
-console.log(screenWidth)
-console.log(screenHeight)
+// console.log(screenWidth);
+// console.log(screenHeight);
 
-const windowInnerWidth = window.innerWidth
-const windowInnerHeight = window.innerHeight
+// const windowInnerWidth = window.innerWidth
+// const windowInnerHeight = window.innerHeight
 
 
-console.log(windowInnerWidth)
-console.log(windowInnerHeight)
+// console.log(windowInnerWidth)
+// console.log(windowInnerHeight)
